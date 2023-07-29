23917f1e344661a428d67454c9d5b1c94a77a6cd
robiulalam76 <robiulalam762400@gmail.com>
2023-07-29 11:23:57 +0600

    redux added and more updated

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,16 @@
 import { Footer } from "@/components/shared/Footer";
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import { Provider } from "react-redux";
+import store from "../../redux/store";
 
 export default function App({ Component, pageProps }) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
-      <Footer />
-    </SessionProvider>
+    <Provider store={store}>
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+        <Footer />
+      </SessionProvider>
+    </Provider>
   );
 }
