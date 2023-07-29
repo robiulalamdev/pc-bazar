68d0df07db79b5f1e9f78840f8833faa85b5304e
robiulalam76 <robiulalam762400@gmail.com>
2023-07-29 12:27:36 +0600

    updated

--- src/pages/pc-builder.js
+++ src/pages/pc-builder.js
@@ -1,5 +1,24 @@
+import Navbar from "../components/shared/Navbar";
 import React from "react";
 
-export default function PcBuilderPage() {
-  return <div>PcBuilderPage</div>;
+export default function PcBuilderPage({ categories }) {
+  return (
+    <div>
+      <Navbar categories={categories?.data} />
+      <div className="min-h-screen"></div>
+    </div>
+  );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    `https://pc-bazar.vercel.app/api/categories/all-categories`
+  );
+  const data = await res.json();
+
+  return {
+    props: {
+      categories: data,
+    },
+  };
+};
